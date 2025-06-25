19ce18fb0395f863a9252fa99995b27765b75cbb
xtr3yx <62310114+xtr3yx@users.noreply.github.com>
2025-06-25 17:44:14 +0200

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,26 @@
+export const quotes = [
+  {
+    text: "Being confident and believing in your own self-worth is necessary to achieving your potential.",
+    author: "Sheryl Sandberg",
+  },
+  {
+    text: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    text: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    text: "Happiness is not something ready made. It comes from your own actions.",
+    author: "Dalai Lama",
+  },
+  {
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    text: "Start where you are. Use what you have. Do what you can.",
+    author: "Arthur Ashe",
+  },
+];
